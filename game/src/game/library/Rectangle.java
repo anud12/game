e34533216f28ba35fc2ea20b568dbc70f0b8c21b
anud12 <supstanta_andu@yahoo.com>
2015-08-09 22:38:50 +0200

    Revert "Revert "Removing old tests""
    This reverts commit f2ec14022bbeb87cd5a09452f6b6c30c4d1a1808.

--- game/src/game/library/Rectangle.java
+++ game/src/game/library/Rectangle.java
@@ -2,9 +2,79 @@ package game.library;
 
 public class Rectangle
 {
-
+    private Point vertices[];   
+    
+    public Point[] getVertices()
+    {
+        return vertices;
+    }
+    
+    public void setWidth(int width)
+    {
+         vertices[0].y = center.y - width/2;
+         vertices[1].y = center.y + width/2;
+         vertices[2].y = center.y + width/2;    
+         vertices[3].y = center.y - width/2;        
+    }
+    
+    public void setHeight(int height)
+    {
+        vertices[0].x = center.x + height/2;
+        vertices[1].x = center.x + height/2;
+        vertices[2].x = center.x - height/2;
+        vertices[3].x = center.x - height/2;
+    }
+    private Point center;
+    
+    public Point getCenter()
+    {
+        return center;
+    }
+    
+    public Rectangle(int width, int height)
+    {
+         vertices = new Point[4];
+         
+         center = new Point();
+         
+         vertices[0] = new Point();
+         vertices[0].x = center.x + height/2;
+         vertices[0].y = center.y - width/2;
+         
+         vertices[1] = new Point();
+         vertices[1].x = center.x + height/2;
+         vertices[1].y = center.y + width/2;
+         
+         vertices[2] = new Point();
+         vertices[2].x = center.x - height/2;
+         vertices[2].y = center.y + width/2;
+         
+         vertices[3] = new Point();
+         vertices[3].x = center.x - height/2;
+         vertices[3].y = center.y - width/2;
+    }
+    
     public Rectangle()
     {
+        vertices = new Point[4];
          
+        center = new Point();
+        
+        for(int i = 0 ; i < 4 ; i++)
+        {
+            vertices[i] = new Point();
+        }
+    }
+    
+    public void transformMove(int x, int y)
+    {
+        center.x += x;
+        center.y += y;
+        
+        for(int i = 0 ; i < 4 ; i++)
+        {
+            vertices[i].x += x;
+            vertices[i].y += y;
+        }
     }
 }
